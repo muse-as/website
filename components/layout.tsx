3a776a0853d0700694155bb1e6efeef5ff148ad5
Sira Sujjinanont <hunt@jitta.com>
2022-05-15 20:56:42 +0700

    Add google analytics

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,8 @@
 import Alert from './alert'
 import Footer from './footer'
 import Meta from './meta'
+import Script from 'next/script'
+import { GA_ID } from '../lib/constants'
 
 type Props = {
   preview?: boolean
@@ -16,6 +18,20 @@ const Layout = ({ preview, children }: Props) => {
         <main>{children}</main>
       </div>
       <Footer />
+      {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${GA_ID}');
+        `}
+      </Script>
     </>
   )
 }
